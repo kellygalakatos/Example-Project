d5832f9e4abe695006d11bca7aaf1d7dc9ba1b77
Kelly Galakatos <kellygalakatos@mwc-024215.dhcp.missouri.edu>
2018-09-28 05:06:32 -0500

    Initial Commit

---
+++ ExampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleProject
+//
+//  Created by Kelly Galakatos on 9/28/18.
+//  Copyright © 2018 Kelly Galakatos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
